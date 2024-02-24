a6c9ca23166de0b19bed97ce202813d41ec3808a
rupalishelar12 <161108710+rupalishelar12@users.noreply.github.com>
2024-02-24 16:31:42 +0530

    Add files via upload
    Project-3
    This all about Employee Management System Project.

---
+++ springboot-crud-project/src/main/java/com/example/springboot/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.example.springboot.service;
+
+import java.util.List;
+
+import com.example.springboot.model.Employee;
+
+public interface EmployeeService {
+	List<Employee> getAllEmployees();
+	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	void deleteEmployeeById(long id);
+	void detailsEmployeeById(long id);
+}
